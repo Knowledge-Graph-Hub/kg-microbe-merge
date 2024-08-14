53bf528e6fae74768af1466a25d5c80a34f55c42
Harshad Hegde <hegdehb@gmail.com>
2024-08-13 19:37:05 -0500

    no neeed to cap memory limit

--- kg_microbe_merge/utils/duckdb_utils.py
+++ kg_microbe_merge/utils/duckdb_utils.py
@@ -422,7 +422,7 @@ def duckdb_edges_merge(edges_file_list, output_file, batch_size=1000000):
     try:
         # Enable memory-mapped storage for temporary tables
         conn.execute(f"PRAGMA temp_directory='{TMP_DIR}'")  # Store temp files in the same directory
-        conn.execute("PRAGMA memory_limit='4GB'")  # Adjust based on available system memory
+        # conn.execute("PRAGMA memory_limit='4GB'")  # Adjust based on available system memory
 
         # Load the files into DuckDB, excluding the 'id' column
         load_into_duckdb(conn, edges_file_list, "combined_edges", exclude_columns=["id"])
